5571a91d2874a48bb05432766717d8923628c7d6
Erica Marigold <hi@devcomp.xyz>
2024-02-15 16:43:37 +0530

    initial v2 configs

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"hardhackerlabs/theme-vim",
+		name = "hardhacker",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.hardhacker_darker = 0
+			vim.g.hardhacker_hide_tilde = 1
+			vim.g.hardhacker_keyword_italic = 1
+			-- vim.cmd("colorscheme hardhacker")
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- vim.cmd("colorscheme tokyonight-storm")
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine-main")
+		end,
+	},
+}
